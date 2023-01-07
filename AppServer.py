b5b031fed66e08dceba6be8e9f20ffa297c1669e
iap-server-git <105693042+iap-server-git@users.noreply.github.com>
2023-01-07 17:48:28 +0800

    Update AppServer.py
    Optimize examples

--- AppServer.py
+++ AppServer.py
@@ -82,7 +82,8 @@ class AppServer:
                 pass
         except (JSONDecodeError, TypeError) as e:
             print('error: %s' % e)
-
+        # When you receive a notification, it is recommended that you request the IAP server to verify the purchased Token according to the purchased Token in the notification content
+        # In order to avoid financial loss, it is necessary to verify the consistency of productId, price, currency and other information in InAppPurchaseData, and deliver the goods after verification
 
 class StatusUpdateNotificationRequest:
     statusUpdateNotification = None
