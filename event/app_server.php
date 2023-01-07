b9dcdf77e3fe6c980b08101445085caf145747c1
iap-server-git <105693042+iap-server-git@users.noreply.github.com>
2023-01-07 17:47:20 +0800

    Update app_server.php
    Optimize examples

--- event/app_server.php
+++ event/app_server.php
@@ -74,6 +74,8 @@ class AppServer{
             case  NOTIFICATION_TYPE::DEFERRED:
                 break;
         }
+        // When you receive a notification, it is recommended that you request the IAP server to verify the purchased Token according to the purchased Token in the notification content
+        // In order to avoid financial loss, it is necessary to verify the consistency of productId, price, currency and other information in InAppPurchaseData, and deliver the goods after verification
     }
 }
 
